d3e2bae77ca6b29c714676c3423ac2d7ad7ae89e
Jeff <jcui19@jhu.edu>
2024-05-01 21:27:57 -0400

    download preview for songs

--- get_spotify_recomendation.py
+++ get_spotify_recomendation.py
@@ -1,4 +1,6 @@
 import os
+import tqdm
+import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
@@ -21,7 +23,33 @@ def auth():
 def get_spotify_recommendations(seed_tracks, num_tracks=20):
     sp = auth()
     related_tracks = []
-    for track_id in seed_tracks:
+    for track_id in tqdm.tqdm(seed_tracks, desc='Getting recommendations'):
         results = sp.recommendations(seed_tracks=[track_id], limit=num_tracks)
         related_tracks.extend([(track['id'], track['name']) for track in results['tracks']])
+    for track in related_tracks:
+        if not download_preview(track[0]):
+            related_tracks.remove(track)
+
+    print(f'Found {len(related_tracks)} related tracks')
     return related_tracks
+
+
+def download_preview(track_id, output_dir='data/spotify_previews'):
+    sp = auth()
+    track = sp.track(track_id)
+    preview_url = track['preview_url']
+    if preview_url is None:
+        return
+    os.makedirs(output_dir, exist_ok=True)
+    preview_file = os.path.join(output_dir, f'{track_id}.mp3')
+
+    response = requests.get(preview_url)
+    if response.status_code == 200:
+        with open(preview_file, 'wb') as file:
+            file.write(response.content)
+        return True
+    else:
+        return False
+
+
+get_spotify_recommendations(['2dYV68n5uCXD5nM8BWQ7kp'])
